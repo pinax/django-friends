dbfc21689286e38a5f286abe23c0dc0d4c0ec212
Alex Gaynor <alex.gaynor@gmail.com>
2009-10-27 12:21:56 -0400

    Added a view for responding to friendship requests.

--- friends/views.py
+++ friends/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 
 from friends.models import FriendshipInvitation
@@ -17,6 +17,22 @@ def find_friends(request):
     return render_to_response("friends/find_friends.html", {
         "users": users,
     }, context_instance=RequestContext(request))
+
+@login_required
+def respond_to_friendship_invitation(request, invitation_id, redirect_to_view=None):
+    inv = get_object_or_404(FriendshipInvitation, to_user=request.user,
+        pk=invitation_id)
+    response = request.GET.get("response", "a")
+    if response == "a":
+        inv.accept()
+    elif response == "d":
+        inv.decline()
+    else:
+        raise ValueError("%s isn't a valid response to a friendship request %s" % response)
+    if redirect_to_view is not None:
+        return redirect(redirect_to_view)
+    return HttpResponseRedirect(request.META["HTTP_REFERER"])
+
 
 """
 @login_required
